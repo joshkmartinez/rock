af514c9d289ebc369713d2cf6743d2019cd3d8e4
Joshua Martinez <joshkmartinez@gmail.com>
2019-03-20 16:22:37 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+
+const api = express.Router()
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+api.get(['/hello', '/hi'], (req, res) => {
+  res.send('hi')
+})
+api.get(['/ping', '/pong'], (req, res) => {
+  res.send('pong')
+})
+api.get('/', (req, res) => {
+  res.sendFile('/static/r.jpg', { root: __dirname })
+})
+app.use('/', api)
+app.listen(3000, () => {
+  //console.log('Server running on port 3000')
+})
